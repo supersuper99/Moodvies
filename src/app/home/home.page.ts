4eea095e30acf7610bf24171849da1efd47164d1
PAPI <Eandrews100@gmail.com>
2023-01-07 13:54:36 -0800

    added suggestion function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  moodForm: FormGroup;
+  moods = [
+    'happy',
+    'sad',
+    'romantic',
+    'adventurous',
+    'exciting',
+    'relaxing',
+    'thought-provoking',
+    'comedic',
+    'dramatic',
+    'action-packed',
+  ];
+  movie: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.moodForm = new FormGroup({
+      mood: new FormControl('', Validators.required),
+    });
+  }
+
+  async suggestMovie() {
+    const mood = this.moodForm.value.mood;
+    const tmdbApiKey = 'YOUR_TMDB_API_KEY';
+    const tmdbUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${tmdbApiKey}&sort_by=popularity.desc&with_genres=${this.getGenreIdFromMood(mood)}`;
+    try {
+      const res = await this.http.get(tmdbUrl).toPromise();
+      this.movie = res.results[0];
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
